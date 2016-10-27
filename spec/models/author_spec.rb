e86791ed08be9ad260af988ea91c83b92f689952
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2016-10-27 20:43:58 +0200

    Corrected failing test for #8

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Author, type: :model do
   it "should return the full name" do
-    except(build(:author).name).to eq('Alan Turing')
+    expect(build(:author).name).to eq('Alan Turing')
   end
 end
