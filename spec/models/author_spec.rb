8b388f1ee3ce874bc28cb80890272f1b322201a5
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2016-10-29 01:49:34 +0200

    Add tests for empty last name

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -4,4 +4,12 @@ RSpec.describe Author, type: :model do
   it "should return the full name" do
     expect(build(:author).name).to eq('Alan Turing')
   end
+
+  it 'should fail when entering no last name (empty string)' do
+    author = Author.new(first_name: "Alan", last_name: "", homepage: "https://de.wikipedia.org/wiki/Alan_Turing").should_not be_valid
+  end
+
+  it 'should fail when entering no last name (no field)' do
+    author = Author.new(first_name: "Alan", homepage: "https://de.wikipedia.org/wiki/Alan_Turing").should_not be_valid
+  end
 end
