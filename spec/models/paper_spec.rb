b9da6915727b28bc4fd3eaf0f23789ed4a37b9b9
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2016-10-29 23:28:38 +0200

    Add validation for integer year

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -36,9 +36,5 @@ RSpec.describe Paper, type: :model do
   it 'should fail when entering non-integer year (characters)' do
     paper = expect(Paper.new(title: 'COMPUTING MACHINERY AND INTELLIGENCE', venue: 'Mind 49: 433-460', year: 'nineteen-fifty')).to_not be_valid
   end
-
-  it 'should fail when entering no year (integer as string)' do
-    paper = expect(Paper.new(title: 'COMPUTING MACHINERY AND INTELLIGENCE', venue: 'Mind 49: 433-460', year: '1950')).to_not be_valid
-  end
 end
 
