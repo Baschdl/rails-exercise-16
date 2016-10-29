42a639582137008974ac7dfcdb56a3af717a3724
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2016-10-29 10:44:48 +0200

    Add validation for empty last name

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,8 @@
 class Author < ActiveRecord::Base
+  validates :last_name, presence: true, length: { minimum: 2 }
+
   def name
+    # we need this, because we have empty entries in our database
     if !first_name.nil? and !last_name.nil?
       return first_name + ' ' + last_name
     end
