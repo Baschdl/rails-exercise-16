548bc42bc8fefdb31284340e2fe56488b2e6deba
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2016-10-29 23:17:36 +0200

    Add failing tests for validation without venue

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe Paper, type: :model do
   it 'should fail when entering no title (no field)' do
     paper = expect(Paper.new(venue: "Mind 49: 433-460", year: 1950)).to_not be_valid
   end
+
+  it 'should fail when entering no venue (empty string)' do
+    paper = expect(Paper.new(title: "COMPUTING MACHINERY AND INTELLIGENCE", venue: "", year: 1950)).to_not be_valid
+  end
+
+  it 'should fail when entering no venue (no field)' do
+    paper = expect(Paper.new(title: "COMPUTING MACHINERY AND INTELLIGENCE", year: 1950)).to_not be_valid
+  end
 end
