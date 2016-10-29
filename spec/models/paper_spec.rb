5bc45a3e72c5f7cb81d3ed23082485cbf2878db2
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2016-10-29 23:24:26 +0200

    Add failing tests for non-integer year

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -2,26 +2,43 @@ require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
   it 'should fail when entering no title (empty string)' do
-    paper = expect(Paper.new(title: "", venue: "Mind 49: 433-460", year: 1950)).to_not be_valid
+    paper = expect(Paper.new(title: '', venue: 'Mind 49: 433-460', year: 1950)).to_not be_valid
   end
 
   it 'should fail when entering no title (no field)' do
-    paper = expect(Paper.new(venue: "Mind 49: 433-460", year: 1950)).to_not be_valid
+    paper = expect(Paper.new(venue: 'Mind 49: 433-460', year: 1950)).to_not be_valid
   end
 
   it 'should fail when entering no venue (empty string)' do
-    paper = expect(Paper.new(title: "COMPUTING MACHINERY AND INTELLIGENCE", venue: "", year: 1950)).to_not be_valid
+    paper = expect(Paper.new(title: 'COMPUTING MACHINERY AND INTELLIGENCE', venue: '', year: 1950)).to_not be_valid
   end
 
   it 'should fail when entering no venue (no field)' do
-    paper = expect(Paper.new(title: "COMPUTING MACHINERY AND INTELLIGENCE", year: 1950)).to_not be_valid
+    paper = expect(Paper.new(title: 'COMPUTING MACHINERY AND INTELLIGENCE', year: 1950)).to_not be_valid
   end
 
   it 'should fail when entering no year (empty string)' do
-    paper = expect(Paper.new(title: "COMPUTING MACHINERY AND INTELLIGENCE", venue: "Mind 49: 433-460")).to_not be_valid
+    paper = expect(Paper.new(title: 'COMPUTING MACHINERY AND INTELLIGENCE', venue: 'Mind 49: 433-460')).to_not be_valid
   end
 
   it 'should fail when entering no year (no field)' do
-    paper = expect(Paper.new(title: "COMPUTING MACHINERY AND INTELLIGENCE", venue: "Mind 49: 433-460")).to_not be_valid
+    paper = expect(Paper.new(title: 'COMPUTING MACHINERY AND INTELLIGENCE', venue: 'Mind 49: 433-460')).to_not be_valid
+  end
+
+  it 'should fail when entering no year (empty string)' do
+    paper = expect(Paper.new(title: 'COMPUTING MACHINERY AND INTELLIGENCE', venue: 'Mind 49: 433-460', year:nil)).to_not be_valid
+  end
+
+  it 'should fail when entering no year (no field)' do
+    paper = expect(Paper.new(title: 'COMPUTING MACHINERY AND INTELLIGENCE', venue: 'Mind 49: 433-460')).to_not be_valid
+  end
+
+  it 'should fail when entering non-integer year (characters)' do
+    paper = expect(Paper.new(title: 'COMPUTING MACHINERY AND INTELLIGENCE', venue: 'Mind 49: 433-460', year: 'nineteen-fifty')).to_not be_valid
+  end
+
+  it 'should fail when entering no year (integer as string)' do
+    paper = expect(Paper.new(title: 'COMPUTING MACHINERY AND INTELLIGENCE', venue: 'Mind 49: 433-460', year: '1950')).to_not be_valid
   end
 end
+
