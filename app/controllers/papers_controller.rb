4e2d2b34127ab34baabc3701c85e6685c12b898b
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2016-11-01 18:17:41 +0100

    Paper page saves the paper

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -1,5 +1,24 @@
 class PapersController < ApplicationController
+  def show
+    @paper = Paper.find(params[:id])
+  end
+
   def new
     @paper = Paper.new
+  end
+
+  def create
+    @paper = Paper.new(paper_params)
+
+    if @paper.save
+      redirect_to @paper
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def paper_params
+    params.require(:paper).permit(:title, :venue, :year)
   end
 end
